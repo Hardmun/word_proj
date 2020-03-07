bc8e892cb473acbd9e643176161a09c43880d586
Hardmun <spamnvg@gmail.com>
2020-03-07 06:30:21 +0300

    lunamoon

--- word.py
+++ word.py
@@ -1,5 +1,11 @@
 from common import full_path
 import docx
+import subprocess
+import os
+import unoconv
 
-wrd = docx.Document(full_path("files/1.docx"))
-fdf=0
+for fl in os.listdir(os.getcwd()):
+    if fl.endswith('.doc'):
+        print(fl)
+        subprocess.call(['d:\My documents\Python\word_proj\unoconv', '-d', 'document', '--format=docx', fl], shell=True)
+
