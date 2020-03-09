8d89858fc8ccd11746a64b0a8a08a32d505c26b9
Hardmun <spamnvg@gmail.com>
2020-03-09 07:20:11 +0300

    lunamoon

--- wordsplit.py
+++ wordsplit.py
@@ -18,8 +18,11 @@ if configList.__len__() == 0:
         config.write(configfile)
 
 """Writing the logs"""
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+loggerError = logging.getLogger("error")
+loggerError.setLevel(logging.ERROR)
+
+loggerInfo = logging.getLogger("info")
+loggerInfo.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s:%(message)s")
 
@@ -29,73 +32,74 @@ if not os.path.isdir(logDir):
     os.mkdir(logDir)
 
 infoHandler = logging.FileHandler(os.path.join("Logs", "info.log"))
+infoHandler.setLevel(logging.INFO)
 infoHandler.setFormatter(formatter)
 
 errorHandler = logging.FileHandler(os.path.join("Logs", "errors.log"))
 errorHandler.setLevel(logging.raiseExceptions)
 errorHandler.setFormatter(formatter)
 
-logger.addHandler(infoHandler)
-logger.addHandler(errorHandler)
+loggerError.addHandler(errorHandler)
+loggerInfo.addHandler(infoHandler)
 
 def logDecorator(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except:
-            logger.exception(f"An error has been occurred in function {func.__name__}")
+        except BaseException as e:
+            loggerError.exception(f"An error has been occurred in function {func.__name__}", exc_info=e)
 
     return wrapper
 
 # @logDecorator
-def splitWordFile(filePath):
-    pass
-    # word = Document(filePath)
-    # rows = word.tables[0].rows
-    #
-    # tbl = word.tables[0]._tbl
-    # tr = rows[13]._tr
-    # tbl.remove(tr)
-    #
-    # for row in rows:
-    #     pass
+# def splitWordFile(filePath):
+#     pass
+# word = Document(filePath)
+# rows = word.tables[0].rows
+#
+# tbl = word.tables[0]._tbl
+# tr = rows[13]._tr
+# tbl.remove(tr)
+#
+# for row in rows:
+#     pass
 
-    # cells = word.tables[0]._cells
-    # for cell in cells:
-    #     if not cell.text.strip() == "":
-    #         print(cell.text)
-    # word.save(os.path.join(os.path.dirname(filePath), "mod.docx"))
+# cells = word.tables[0]._cells
+# for cell in cells:
+#     if not cell.text.strip() == "":
+#         print(cell.text)
+# word.save(os.path.join(os.path.dirname(filePath), "mod.docx"))
 
-# for fl in os.listdir(os.getcwd()):
-#     if fl.endswith('.doc'):
-#         print(fl)
-#         wrd = client.Dispatch("Word.Application")
-#         wrd.visible = 0
-#         doc = wrd.Documents.Open(full_path("22.doc"))
-#         doc.SaveAs(full_path("testconvert"), FileFormat=12)
-#         doc.Close()
-#         wrd.Quit()
-
+def messageFile(txtList, msgDir):
+    with open(os.path.join(msgDir, "message.txt"), "w", encoding="utf-8") as file:
+        file.write("\n".join(txtList))
 
 def docToDocx(filePath):
     import pythoncom
     from win32com import client
     pythoncom.CoInitialize()
-    isConverted = True
     """checking microsoft word was installed"""
     try:
         wrd = client.Dispatch("Word.Application")
-    except:
-        logger.exception("The Word application hasn't been installed!")
+    except BaseException as errMsg:
+        loggerError.exception("The Word application hasn't been installed!")
+        fileDir = os.path.dirname(filePath)
+        messageFile(["Не обнаружено установленой программы WORD на вашем компьютере!",
+                     str(errMsg)], fileDir)
         return False
 
+    fileDir = os.path.dirname(filePath)
     wrd.visible = 0
     try:
+        newFileName = os.path.splitext(filePath)[0]
         doc = wrd.Documents.Open(filePath)
-        doc.SaveAs(os.path.join(os.path.dirname(filePath), os.path.splitext(filePath)[0]), FileFormat=12)
+        doc.SaveAs(os.path.join(fileDir, newFileName), FileFormat=12)
         doc.Close()
-    except:
-        logger.exception("The Word application hasn't been opened or saved!")
+        isConverted = f"{newFileName}.docx"
+    except BaseException as errMsg:
+        loggerError.exception("The Word application hasn't been opened or saved!")
+        messageFile(["Ошибка открытия или сохранения файла. Закройте все приложения WORD на вашем компьютере!",
+                     str(errMsg)], fileDir)
         isConverted = False
 
     wrd.Quit()
@@ -104,12 +108,20 @@ def docToDocx(filePath):
 class WordHandler(FileSystemEventHandler):
     def on_created(self, event):
         """path to file"""
-        filePath = event.src_path
-        fileDir = os.path.dirname(filePath)
-        for file in os.listdir(fileDir):
-            """converting *.doc into *.docx"""
+        file = event.src_path
+        fileDir = os.path.dirname(file)
+        # for file in os.listdir(fileDir):
+        """converting *.doc into *.docx"""
+        if file.find("~$") == -1:
+            """deleting message.txt"""
+            if os.path.basename(file) != "message.txt":
+                msgPath = os.path.join(fileDir, "message.txt")
+                if os.path.exists(msgPath):
+                    os.unlink(msgPath)
             if file.endswith(".doc"):
-                fileConverted = docToDocx(os.path.normpath(os.path.join(fileDir, file)))
+                newFile = docToDocx(os.path.normpath(os.path.join(fileDir, file)))
+                if newFile:
+                    loggerInfo.info(f"The file {file} was successfully converted to {newFile}")
             elif file.endswith(".docx"):
                 splitWordFile(os.path.normpath(os.path.join(fileDir, file)))
 
@@ -121,10 +133,10 @@ class IniHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.src_path.find("settings.ini") != -1:
             config.read("settings.ini")
-            observer = self.obs
+            obsr = self.obs
             newPath = config.get("Paths", "Path")
-            observer.schedule(WordHandler(), path=os.path.normpath(newPath))
-            logger.info(f'The directory has been changed to {newPath}')
+            obsr.schedule(WordHandler(), path=os.path.normpath(newPath))
+            loggerInfo.info(f'The directory has been changed to {newPath}')
 
 if __name__ == '__main__':
     """directory for observing"""
@@ -138,12 +150,12 @@ if __name__ == '__main__':
     observerINI.schedule(IniHandler, path=projectDir)
     observerINI.start()
 
-    try:
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        observer.stop()
-        observerINI.stop()
-
+    # try:
+    #     while True:
+    #         time.sleep(10)
+    # except KeyboardInterrupt:
+    #     observer.stop()
+    #     observerINI.stop()
+    #
     observer.join()
     observerINI.join()
