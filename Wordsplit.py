718c6b97f1342e4b02b5da1d91b5c8310fe4b0d4
ZeroCool <spamnvg@gmail.com>
2020-03-18 15:40:00 +0300

    test

--- Wordsplit.py
+++ Wordsplit.py
@@ -405,10 +405,9 @@ class winService(win32serviceutil.ServiceFramework):
         obsDirectory(self)
 
 if __name__ == '__main__':
-    obsDirectory()
-    # if len(sys.argv) == 1:
-    #     servicemanager.Initialize()
-    #     servicemanager.PrepareToHostSingle(winService)
-    #     servicemanager.StartServiceCtrlDispatcher()
-    # else:
-    #     win32serviceutil.HandleCommandLine(winService)
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(winService)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(winService)
